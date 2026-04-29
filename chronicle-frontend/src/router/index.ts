f9b253b3116517005a3ababffc0335bbb369e7fc
cxin2999 <cxin2999@gmail.com>
2026-04-29 22:25:42 +0800

    chore(router): 配置问题修复

--- chronicle-frontend/src/router/index.ts
+++ chronicle-frontend/src/router/index.ts
@@ -1,20 +1,13 @@
 import RecordPage from '@/pages/RecordPage.vue'
 import HistoryPage from '@/pages/HistoryPage.vue'
 import ProfilePage from '@/pages/ProfilePage.vue'
-import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import UserLoginPage from '@/pages/user/UserLoginPage.vue'
 import UserRegisterPage from '@/pages/user/UserRegisterPage.vue'
 import UserManagePage from '@/pages/admin/UserManagePage.vue'
 
-
-// 读取打包模式：Cordova 用 hash，Web 用 history
-const historyMode =
-  import.meta.env.VITE_ROUTER_MODE === 'hash'
-    ? createWebHashHistory(import.meta.env.BASE_URL)
-    : createWebHistory(import.meta.env.BASE_URL)
-
 const router = createRouter({
-  history: historyMode,
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -54,4 +47,3 @@ const router = createRouter({
 })
 
 export default router
-
--- chronicle-frontend/vite.config.ts
+++ chronicle-frontend/vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: './',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
